25afbb7b2653e44d914c99466615f9e508119a8f
gaju91 <gajanand742@gmail.com>
2025-04-12 13:10:14 +0530

    ➕ add(): server route for video streaming

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const videoHandler = require('./video-stream');
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/video', videoHandler);
 
 app.listen(3000, () => {
     console.log(`🟢 Server started on port 3000`)
