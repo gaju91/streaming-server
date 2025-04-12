288845c173e3a5fdc9b1979bd4ebc9d3275bcbe5
gaju91 <gajanand742@gmail.com>
2025-04-12 17:34:58 +0530

    ➕ add(): server route for sse handler

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const path = require('path');
 
 const videoHandler = require('./video-stream');
 const chunkHandler = require('./chunked-json');
+const sseHandler = require('./server-side-events');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/video', videoHandler);
 
 app.use('/chunked', chunkHandler);
+
+app.use('/events', sseHandler);
 
 app.listen(3000, () => {
     console.log(`🟢 Server started on port 3000`)
