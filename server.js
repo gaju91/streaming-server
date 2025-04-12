6a0618216679229bbc4e7210696c36426ede2f9d
gaju91 <gajanand742@gmail.com>
2025-04-12 15:49:02 +0530

    ➕ add(): server route for chunked data

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const app = express();
 const path = require('path');
 
 const videoHandler = require('./video-stream');
+const chunkHandler = require('./chunked-json');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/video', videoHandler);
+
+app.use('/chunked', chunkHandler);
 
 app.listen(3000, () => {
     console.log(`🟢 Server started on port 3000`)
